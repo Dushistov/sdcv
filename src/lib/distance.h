482feda09ef06f2cf6b8aa82c79a5b64cae26bbc
Evgeniy Dushistov <dushistov@mail.ru>
2013-07-06 22:56:20 +0000

    distance: c++11 plus remove not used code

--- src/lib/distance.h
+++ src/lib/distance.h
@@ -1,26 +1,23 @@
-#ifndef DISTANCE_H
-#define DISTANCE_H
+#pragma once
 
+#include <cstdlib>
 #include <glib.h>
 
 class EditDistance {
+public:
+    EditDistance() {
+        currentelements = 2500; // It's enough for most conditions :-)
+        d = static_cast<int *>(malloc(sizeof(int)*currentelements));
+    }
+    ~EditDistance() {
+        if (d != nullptr)
+            free(d);
+    }
+    EditDistance(const EditDistance&) = delete;
+    EditDistance& operator=(const EditDistance&) = delete;
+    int CalEditDistance( const gunichar *s, const gunichar *t, const int limit );
 private:
     int *d;
     int currentelements;
-    /*Gets the minimum of three values */
-    inline int minimum( const int a, const int b, const int c )
-    {
-        int min = a;
-        if ( b < min )
-              min = b;
-        if ( c < min )
-              min = c;
-          return min;
-    };
-public:
-    EditDistance(  );
-    ~EditDistance(  );
-    int CalEditDistance( const gunichar *s, const gunichar *t, const int limit );
 };
 
-#endif
