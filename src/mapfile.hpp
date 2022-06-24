6f30be7815fdc088fc441afe9a8663f0541ff653
Evgeniy A. Dushistov <dushistov@mail.ru>
2022-06-24 21:17:31 +0300

    clang-format for mapfile

--- src/mapfile.hpp
+++ src/mapfile.hpp
@@ -40,20 +40,18 @@ inline bool MapFile::open(const char *file_name, unsigned long file_size)
     size = file_size;
 #ifdef HAVE_MMAP
     if ((mmap_fd = ::open(file_name, O_RDONLY)) < 0) {
-        //g_print("Open file %s failed!\n",fullfilename);
+        // g_print("Open file %s failed!\n",fullfilename);
         return false;
     }
     data = (gchar *)mmap(nullptr, file_size, PROT_READ, MAP_SHARED, mmap_fd, 0);
     if ((void *)data == (void *)(-1)) {
-        //g_print("mmap file %s failed!\n",idxfilename);
+        // g_print("mmap file %s failed!\n",idxfilename);
         data = nullptr;
         return false;
     }
 #elif defined(_WIN32)
-    hFile = CreateFile(file_name, GENERIC_READ, 0, nullptr, OPEN_ALWAYS,
-                       FILE_ATTRIBUTE_NORMAL, 0);
-    hFileMap = CreateFileMapping(hFile, nullptr, PAGE_READONLY, 0,
-                                 file_size, nullptr);
+    hFile = CreateFile(file_name, GENERIC_READ, 0, nullptr, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0);
+    hFileMap = CreateFileMapping(hFile, nullptr, PAGE_READONLY, 0, file_size, nullptr);
     data = (gchar *)MapViewOfFile(hFileMap, FILE_MAP_READ, 0, 0, file_size);
 #else
     gsize read_len;
